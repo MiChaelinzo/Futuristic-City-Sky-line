d63c0779e0a80c8f05c705ac67f8ac8143d738e8
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-01-13 09:24:41 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,24 @@
+// -----JS CODE-----
+// First, we create a new scene
+const scene = new Scene();
+
+// Next, we create a text object that will display the current time
+const text = new Text();
+text.value = "00:00:00";
+text.fontSize = 60;
+text.position = new Vector3(0, 0, 0);
+scene.root.children.add(text);
+
+// We need to update the text object's value every second
+const updateTime = () => {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  text.value = `${hours}:${minutes}:${seconds}`;
+};
+setInterval(updateTime, 1000);
+
+// Finally, we set the scene to be the active scene
+scene.root.children.add(text);
+Engine.setCurrentScene(scene);
